e63641d64166809d49e301d4f9b708c57ec411b3
Venyamin Shvets <venyashvets@gmail.com>
2024-05-15 16:46:40 +0300

    u

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,20 @@ import { createServer } from "http";
 const backendUrl = "https://api.themoviedb.org";
 
 createServer((req, res) => {
+  if (req.url === "/favicon.ico") {
+    res.end();
+    return;
+  }
   console.log(req.url);
   res.setHeader("content-type", "application/json");
   fetch(backendUrl + req.url!).then(
-    (r) => {
+    async (r) => {
       console.log(r);
-      res.end(JSON.stringify(r));
+      res.end(await r.json());
     },
     (e) => {
       console.log(e);
-      res.end(JSON.stringify(e));
+      res.end();
     },
   );
 }).listen(8000);
