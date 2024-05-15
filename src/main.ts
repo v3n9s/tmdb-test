b9e4ef935ce1b4f066ebd15f85c7d77f6d8f8e5d
Venyamin Shvets <venyashvets@gmail.com>
2024-05-15 16:41:30 +0300

    u

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ const backendUrl = "https://api.themoviedb.org";
 
 createServer((req, res) => {
   console.log(req.url);
+  res.setHeader("content-type", "application/json");
   fetch(backendUrl + req.url!).then(
     (r) => {
       console.log(r);
+      res.end(JSON.stringify(r));
     },
     (e) => {
       console.log(e);
+      res.end(JSON.stringify(e));
     },
   );
-  res.end();
 }).listen(8000);
