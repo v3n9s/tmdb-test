facd18c027341f888d75d6a6a05bfe9ede817803
Venyamin Shvets <venyashvets@gmail.com>
2024-05-20 15:29:58 +0300

    Revert "u"
    This reverts commit c5f5b3ad4f672a34ad4cef660ba755cb41f4c4b4.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,36 @@
 import { createServer } from "http";
 
 createServer((req, res) => {
-  res.end();
+  if (req.url === "/favicon.ico") {
+    res.end();
+    return;
+  }
+
+  console.log(req.url);
+
+  const targetUrl = new URL(
+    req.url as string,
+    "http://" +
+      req.headers.host!.replace(
+        new RegExp(`${process.env["HOST"]!}$`),
+        process.env["TARGET_HOST"]!,
+      ),
+  );
+
+  fetch(targetUrl, {
+    headers: {
+      accept: "application/json",
+      authorization: "Bearer " + process.env["API_TOKEN"]!,
+    },
+  }).then(
+    async (r) => {
+      res.writeHead(r.status, [...r.headers]);
+      res.end(await r.text());
+    },
+    (e) => {
+      console.log(e);
+      res.writeHead(500);
+      res.end();
+    },
+  );
 }).listen(process.env["PORT"] ?? 8000);
